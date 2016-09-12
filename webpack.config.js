a763473b15be0add3c248f760218e6638c57e9bb
Guillaume Degermann <guillaume.deger@gmail.com>
2016-09-13 00:46:08 +0200

    Init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/**
+ * Created by guillaumedegermann on 13/09/2016.
+ */
+module.exports = {
+    entry: "./src/js/main.js",
+    output: {
+        path: './dist',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    devServer: {
+        inline: true,
+        contentBase: './dist'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_module|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
